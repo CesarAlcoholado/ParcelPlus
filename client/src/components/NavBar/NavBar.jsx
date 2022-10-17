6765d176d594a70ff36d933807d06ca4a0faa5bc
Cesar <cesar.alcoholado@hotmail.com>
2022-10-17 15:00:32 -0300

    update navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import styles from './NavBar.module.css';
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <div>
+      <div className={styles.LogoContainer}></div>
+      <div className={styles.ListContainer}>
+        <ul className={styles.ul}>
+          <li className={styles.ListItem}>Inicio</li>
+          <li className={styles.ListItem}>Servicios</li>
+          <li className={styles.ListItem}>Presupuesto</li>
+          <li className={styles.ListItem}>Contacto</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
