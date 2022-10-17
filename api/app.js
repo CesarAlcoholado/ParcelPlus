a2f686828cf7cd95d37e3fcab4c7ec33e5570d35
Braian Aguada <braianaguada@gmail.com>
2022-10-17 13:42:19 -0300

    Update Server

---
+++ api/app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+// app.use("/api", require("./routes"));
+
+app.listen(port, () => {
+  console.log(`**CONECTADO AL SERVIDOR, EN EL PUERTO: ${port}**`);
+});
