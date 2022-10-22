394ff49e675480dbf36936e271076f133f6bcbda
Cesar <cesar.alcoholado@hotmail.com>
2022-10-22 13:04:03 -0300

    added axios posts

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar.jsx';
 import Form from '../../components/Form/Form.jsx';
-import Cv from '../../components/Cv/Cv.jsx';
 
 export default function Home() {
   return (
