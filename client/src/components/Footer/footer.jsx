5a94583b8a511d18d03ba733504ef2ec49d99aa3
Braian Aguada <braianaguada@gmail.com>
2022-10-24 11:57:52 -0300

    Upload Footer

---
+++ client/src/components/Footer/footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col">
+                        <h4>Compañia</h4>
+                        <ul>
+                            <li>
+                                <a href="#">Acerda de </a>
+                                <a href="#">Nuestros servicios</a>
+                                <a href="#">Politicas de privacidad</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>Ayuda</h4>
+                        <ul>
+                            <li>
+                                <a href="#"></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>Follow us</h4>
+                        <div class="social-links"></div>
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                    <div class="footer-col">
+                        <h4>LOGO</h4>
+                        <ul>
+                            <li>
+                                <a href="#"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+  );
+}
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Form from '../../components/Form/Form.jsx';
-import Cv from '../../components/Cv/Cv.jsx';
-
+// import Cv from '../../components/Cv/Cv.jsx';
+import Footer from '../../components/Footer/footer.jsx';
 export default function Home() {
   return (
     <div>
       <Form/>
+    <Footer/>
     </div>
   )
 }
