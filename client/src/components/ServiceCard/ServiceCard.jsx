386696e71fffe31f1f77602a08d1c5e524f3feb8
Cesar <cesar.alcoholado@hotmail.com>
2022-10-25 16:16:31 -0300

    Update Services section

---
+++ client/src/components/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./ServiceCard.module.css";
+
+export default function ServiceCard({title, icon, image}) {
+
+  
+  return (
+    <div className={styles.card}>
+      <img src={image} alt="icon" height={"70px"} />
+      <h3 className={styles.title}>{title}</h3>
+    </div>
+  );
+}
