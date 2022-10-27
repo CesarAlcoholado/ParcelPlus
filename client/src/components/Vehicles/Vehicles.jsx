76598b6ab968a65df2a90c974605be8d4f996a3c
Cesar <cesar.alcoholado@hotmail.com>
2022-10-27 16:02:51 -0300

    Update responsive vehicles section

--- client/src/components/Vehicles/Vehicles.jsx
+++ client/src/components/Vehicles/Vehicles.jsx
@@ -1,21 +1,31 @@
 import React from "react";
-import van from "../../static/assets/parked-van.jpg";
+import van from "../../static/assets/float.png";
 import styles from "./Vehicles.module.css"
 
 export default function Vehicles() {
   return (
-    <div className={styles.OurfloatContainer}>
-      <div className={styles.infoContainer}>
-        <h1 className={styles.title}>Nuestra Flota</h1>
-        <p className={styles.description}>
-          Contamos con 11 unidades modernas y en excelentes condiciones
-        </p>
+    <>
+      <div className={styles.OurfloatContainer}>
+        <div className={styles.infoContainer}>
+          <h1 className={styles.title}>Nuestra Flota</h1>
+          <p className={styles.description}>
+            Contamos con 11 unidades modernas y en excelentes condiciones
+          </p>
+        </div>
+        <img
+          className={styles.vehiclesImage}
+          src={van}
+          alt="orange-background"
+          loading="lazy"
+        />
       </div>
-      <img
-        className={styles.vehiclesImage}
-        src={van}
-        alt="orange-background"
-      />
-    </div>
+      <div className={styles.guarantee}>
+        <h1 className={styles.guaranteeTitle}>
+          Garantizamos el 
+          <span className={styles.percentage}> 97% </span>
+          de entregas directas al dia
+        </h1>
+      </div>
+    </>
   );
 }
