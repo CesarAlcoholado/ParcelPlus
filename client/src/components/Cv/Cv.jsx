9117893440f6b24d3b21e3d905b1b9ed9247319c
Braian Aguada <braianaguada@gmail.com>
2022-10-28 23:43:31 -0300

    Update

--- client/src/components/Cv/Cv.jsx
+++ client/src/components/Cv/Cv.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useForm } from "../../static/utils/hooks/useForm";
 import { cvType } from "../../static/constants";
-import styles from "./cv.module.css";
+import styles from "./Cv.module.css";
 
 const initialCv = {
   name: "",
@@ -26,133 +26,142 @@ export default function Cv() {
   );
 
   return (
-    <div className={styles.container}>
-      <div className="content">
-        <form noValidate onSubmit={handleSubmit}>
-          <label>Nombre y Apellido*</label>
-          <input
-            type="text"
-            name="name"
-            value={form.name}
-            onChange={handleChange}
-          />
-          {errors.name && <p>{errors.name}</p>}
-          <label>Edad* (debes ser mayor a 18 años)</label>
-          <input
-            type="number"
-            name="age"
-            value={form.age}
-            onChange={handleChange}
-          />
-          {errors.age && <p>{errors.age}</p>}
-          <label>Provincia*</label>
-          <input
-            type="text"
-            name="region"
-            value={form.region}
-            onChange={handleChange}
-          />
-          {errors.region && <p>{errors.region}</p>}
-          <label>Ciudad*</label>
-          <input
-            type="text"
-            name="city"
-            value={form.city}
-            onChange={handleChange}
-          />
-          {errors.city && <p>{errors.city}</p>}
-          <label>Domicilio*</label>
-          <input
-            type="text"
-            name="homeAdress"
-            value={form.homeAdress}
-            onChange={handleChange}
-          />
-          {errors.homeAdress && <p>{errors.homeAdress}</p>}
-          <label>Email*</label>
-          <input
-            type="email"
-            name="email"
-            value={form.email}
-            onChange={handleChange}
-          />
-          {errors.email && <p>{errors.email}</p>}
-          <label>Celular*</label>
-          <input
-            type="number"
-            name="cellphone"
-            value={form.cellphone}
-            onChange={handleChange}
-          />
-          {errors.cellphone && <p>{errors.cellphone}</p>}
-          <label>Tipo de vehiculo utilitario*</label>
-          <select name="car_type" value={form.car_type} onChange={handleChange}>
-            <option value="0">Seleccione una opción</option>
-            <option value="Auto">Auto</option>
-            <option value="Camioneta">Camioneta</option>
-            <option value="Trafic">Tráfic</option>
-            <option value="Camion">Camión</option>
-          </select>
-          {errors.car_type && <p>{errors.car_type}</p>}
-          <label>Marca*</label>
-          <input
-            type="text"
-            name="car_brand"
-            value={form.car_brand}
-            onChange={handleChange}
-          />
-          {errors.car_brand && <p>{errors.car_brand}</p>}
-          <label>Modelo del vehiculo*</label>
-          <input
-            type="number"
-            name="car_model"
-            value={form.car_model}
-            onChange={handleChange}
-          />
-          {errors.car_model && <p>{errors.car_model}</p>}
-          <label>Posee licencia?*</label>
-          <select name="license" value={form.license} onChange={handleChange}>
-            <option value="0">Seleccione una opción</option>
-            <option value="Si">Sí</option>
-            <option value="No">No</option>
-          </select>
-          {errors.license && <p>{errors.license}</p>}
-          <label>Su póliza está al dia?*</label>
-          <select name="policy" value={form.policy} onChange={handleChange}>
-            <option value="0">Seleccione una opción</option>
-            <option value="Si">Sí</option>
-            <option value="No">No</option>
-          </select>
-          {errors.policy && <p>{errors.policy}</p>}
-          <label>Es titular?*</label>
-          <select name="holder" value={form.holder} onChange={handleChange}>
-            <option value="0">Seleccione una opción</option>
-            <option value="Si">Sí</option>
-            <option value="No">No</option>
-          </select>
-          {errors.holder && <p>{errors.holder}</p>}
-          <input
-            type="submit"
-            value="Enviar"
-            disabled={
-              Object.keys(errors).length ||
-              errors.name ||
-              errors.age ||
-              errors.region ||
-              errors.city ||
-              errors.homeAdress ||
-              errors.email ||
-              errors.cellphone ||
-              errors.car_type ||
-              errors.car_brand ||
-              errors.car_model ||
-              errors.license ||
-              errors.policy ||
-              errors.holder
-            }
-          />
-        </form>
+    <body>
+      <button id="open" className={styles.open}>ABRIR</button>
+
+      <div id="container" className={styles.container}>
+        <div className={styles.content}>
+          <form noValidate onSubmit={handleSubmit}>
+            <label>Nombre y Apellido*</label>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+            />
+            {errors.name && <p>{errors.name}</p>}
+            <label>Edad* (debes ser mayor a 18 años)</label>
+            <input
+              type="number"
+              name="age"
+              value={form.age}
+              onChange={handleChange}
+            />
+            {errors.age && <p>{errors.age}</p>}
+            <label>Provincia*</label>
+            <input
+              type="text"
+              name="region"
+              value={form.region}
+              onChange={handleChange}
+            />
+            {errors.region && <p>{errors.region}</p>}
+            <label>Ciudad*</label>
+            <input
+              type="text"
+              name="city"
+              value={form.city}
+              onChange={handleChange}
+            />
+            {errors.city && <p>{errors.city}</p>}
+            <label>Domicilio*</label>
+            <input
+              type="text"
+              name="homeAdress"
+              value={form.homeAdress}
+              onChange={handleChange}
+            />
+            {errors.homeAdress && <p>{errors.homeAdress}</p>}
+            <label>Email*</label>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+            />
+            {errors.email && <p>{errors.email}</p>}
+            <label>Celular*</label>
+            <input
+              type="number"
+              name="cellphone"
+              value={form.cellphone}
+              onChange={handleChange}
+            />
+            {errors.cellphone && <p>{errors.cellphone}</p>}
+            <label>Tipo de vehiculo utilitario*</label>
+            <select
+              name="car_type"
+              value={form.car_type}
+              onChange={handleChange}
+            >
+              <option value="0">Seleccione una opción</option>
+              <option value="Auto">Auto</option>
+              <option value="Camioneta">Camioneta</option>
+              <option value="Trafic">Tráfic</option>
+              <option value="Camion">Camión</option>
+            </select>
+            {errors.car_type && <p>{errors.car_type}</p>}
+            <label>Marca*</label>
+            <input
+              type="text"
+              name="car_brand"
+              value={form.car_brand}
+              onChange={handleChange}
+            />
+            {errors.car_brand && <p>{errors.car_brand}</p>}
+            <label>Modelo del vehiculo*</label>
+            <input
+              type="number"
+              name="car_model"
+              value={form.car_model}
+              onChange={handleChange}
+            />
+            {errors.car_model && <p>{errors.car_model}</p>}
+            <label>Posee licencia?*</label>
+            <select name="license" value={form.license} onChange={handleChange}>
+              <option value="0">Seleccione una opción</option>
+              <option value="Si">Sí</option>
+              <option value="No">No</option>
+            </select>
+            {errors.license && <p>{errors.license}</p>}
+            <label>Su póliza está al dia?*</label>
+            <select name="policy" value={form.policy} onChange={handleChange}>
+              <option value="0">Seleccione una opción</option>
+              <option value="Si">Sí</option>
+              <option value="No">No</option>
+            </select>
+            {errors.policy && <p>{errors.policy}</p>}
+            <label>Es titular?*</label>
+            <select name="holder" value={form.holder} onChange={handleChange}>
+              <option value="0">Seleccione una opción</option>
+              <option value="Si">Sí</option>
+              <option value="No">No</option>
+            </select>
+            {errors.holder && <p>{errors.holder}</p>}
+            <input
+              type="submit"
+              value="Enviar"
+              disabled={
+                Object.keys(errors).length ||
+                errors.name ||
+                errors.age ||
+                errors.region ||
+                errors.city ||
+                errors.homeAdress ||
+                errors.email ||
+                errors.cellphone ||
+                errors.car_type ||
+                errors.car_brand ||
+                errors.car_model ||
+                errors.license ||
+                errors.policy ||
+                errors.holder
+              }
+            />
+          </form>
+          <button id="close" className={styles.close}>CERRAR</button>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
