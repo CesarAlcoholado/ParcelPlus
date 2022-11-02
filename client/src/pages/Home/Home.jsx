181367cea755e3762d33b5270810c3dc605c91df
Cesar <cesar.alcoholado@hotmail.com>
2022-11-02 14:43:32 -0300

    update before merge

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Contact from "../../components/Contact/Contact.jsx";
 
 export default function Home() {
   return (
     <div>
-      <Contact />
     </div>
   );
 }
