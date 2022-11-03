8e4a0e398953117823a184d4bd29dc4d12684ffd
Cesar <cesar.alcoholado@hotmail.com>
2022-11-03 13:02:01 -0300

    Created Modal component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,10 @@
-import React, {useState} from "react";
-import Cv from "../../components/Cv/Cv.jsx";
+import React from "react";
 
 export default function Home() {
-  const [openModal, setOpenModal] = useState(false);
+  
   return (
     <div>
-      <button onClick={() => setOpenModal(true)}>ABRIR</button>
-      <Cv open={openModal} onClose={() => setOpenModal(false)}/>
+      
     </div>
   );
 }
