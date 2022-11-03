a33c67f771c76d92e87903f097a76ac1b83dc3a8
Cesar <cesar.alcoholado@hotmail.com>
2022-11-03 15:27:26 -0300

    Modal section update

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -5,6 +5,7 @@ export default function Modal () {
   const [openModal, setOpenModal] = useState(false);
   return (
     <div>
+      <h1>Formá parte de nuestro equipo</h1>
       <button onClick={() => setOpenModal(true)}>ABRIR</button>
       <Cv open={openModal} onClose={() => setOpenModal(false)} />
     </div>
