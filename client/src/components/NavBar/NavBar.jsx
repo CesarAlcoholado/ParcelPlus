5b97c50c87f4fc66598836061495529f6b806864
Cesar <cesar.alcoholado@hotmail.com>
2022-11-03 11:30:44 -0300

    added Logo

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MobileNavigation from './MobileNavigation';
 import Navigation from './Navigation';
+import Logo from '../../static/assets/Logo.png'
 import styles from './NavBar.module.css';
 
 export default function NavBar() {
   return (
     <div className={styles.NavBar}>
+      <img src={Logo} alt="ParcelPlusLogo" style={{"width": "60px", "margin-right":"auto"}}/>
      <Navigation/> 
      <MobileNavigation/>
     </div>
