d5c950408f41ba12099cbe1e57f9b1ebc8d9de22
Cesar <cesar.alcoholado@hotmail.com>
2022-11-09 15:04:11 -0300

    Modal styles desktop update

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Cv from '../Cv/Cv.jsx';
 import styles from './Modal.module.css'
 
 export default function Modal () {
   const [openModal, setOpenModal] = useState(false);
+
+  useEffect ( ()=>{
+    if (openModal) {document.body.style.overflow = 'hidden'
+    }else{
+      document.body.style.overflow = 'unset'
+    }
+  }, [openModal])
+
+
   return (
     <div className={styles.modal}>
       <button className={styles.joinButton} onClick={() => setOpenModal(true)}>Postularme</button>
