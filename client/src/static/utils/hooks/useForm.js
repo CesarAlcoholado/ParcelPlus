bc722bd8b6a4cf401ef07af050516f5937d2ee36
Cesar <cesar.alcoholado@hotmail.com>
2022-11-11 13:53:59 -0300

    added post function on submit

--- client/src/static/utils/hooks/useForm.js
+++ client/src/static/utils/hooks/useForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import validate from '../functions/validation';
 import cvValidation from '../functions/cvValidation';
+import { postForm, postCv } from '../../Axios/Axios';
 
 export const useForm = (initialForm, type, notify) => {
   const [form, setForm] = useState(initialForm);
@@ -25,12 +26,22 @@ export const useForm = (initialForm, type, notify) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     if(type === "packageForm"){
       setErrors(validate(form))
     } else  if (type === "cvForm"){
       setErrors(cvValidation(form))
     }
-    if (Object.keys(errors).length) return;
+
+    if (Object.keys(errors).length) {
+      return
+    }else {
+      if (type === "packageForm"){
+        postForm(form)
+      } else if (type === "cvForm"){
+        postCv(form)
+      }
+    };
     notify()
   };
 
