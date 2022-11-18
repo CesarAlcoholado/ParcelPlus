90e5dbfc0846c2eb59fa2d9f4eede412d3da3716
Cesar <cesar.alcoholado@hotmail.com>
2022-11-18 16:15:40 -0300

    cards hover update

--- client/src/static/Axios/Axios.js
+++ client/src/static/Axios/Axios.js
@@ -3,22 +3,24 @@ import axios from 'axios';
 const apiForm = process.env.REACT_API_FORM;
 const apiCv = process.env.REACT_API_CV;
 
-export const postForm = async (form) => {
+export function postForm (form) {
+  return async function(){
   try {
     let postForm = await axios.post(`${apiForm}`, form )
     console.log(postForm);
     return postForm;
   } catch (error){
     console.error(error.message)
-  }
+  }}
 }
 
-export const postCv = async (form) => {
+export function postCv (form) {
+  return async function(){
   try {
     let postCv = await axios.post(`${apiCv}`, form);
     console.log(postCv);
     return postCv;
   } catch (error) {
     console.error(error.message);
-  }
+  }}
 };
