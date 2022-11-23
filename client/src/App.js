d767bdfe3a7d3b5e3c73154ba641dec683a656e3
Cesar <cesar.alcoholado@hotmail.com>
2022-11-23 15:36:16 -0300

    disabled react-dev-tools

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
 import './App.css';
+
+if (process.env.REACT_APP_NODE_ENV === "production") {
+  disableReactDevTools();
+}
 
 function App() {
   return (
